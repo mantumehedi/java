09aa206c30e9f3349a3627323313989425681388
Mantu Mehedi <36851927+mantumehedi@users.noreply.github.com>
2021-03-03 09:55:04 +0530

    Update constructor_test.java

--- constructor_test.java
+++ constructor_test.java
@@ -1,13 +1,25 @@
 class A
 {
 	int x;
+	String s;
 	A()
 	{
 		x = 10;
+		s = "BSc(CS)";
+		startup();
+	}
+	A(int a, String q)
+	{
+		x = a;
+		s = q;
+	}
+	void startup()
+	{
+		System.out.println("Hello, I am a startup");
 	}
 	void show()
 	{
-		System.out.println("Hello "+ x);
+		System.out.println("Hello "+ x +"\t"+ s);
 	}
 }
 
@@ -17,7 +29,11 @@ class constructor_test
 	{
 		A a = new A();
 		a.show();
-		A b = new A();
+		A b = new A(786, "Parameterized constructor");
 		b.show();
+		A c = new A();
+		c.show();
+		A d = new A(1, "Java");
+		d.show();
 	}
 }
